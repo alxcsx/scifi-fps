fc67e2228a7d28563c531063fd4015e10a3b9776
Alex Candido <acs11@cin.ufpe.br>
2026-05-18 09:46:27 -0300

    add animação pistola (wip)

--- scripts/views/BaseWeaponView.gd
+++ scripts/views/BaseWeaponView.gd
@@ -10,7 +10,7 @@ func is_busy() -> bool: return anim_player.is_playing()
 
 func play_shoot_effects() -> void:
   anim_player.stop()
-  if anim_player.has_animation("fire"): anim_player.play("fire")
+  if anim_player.has_animation("shoot"): anim_player.play("shoot")
 
 func play_equip_effects() -> void:
   if anim_player.has_animation("equip"): anim_player.play("equip")
