ea2ec4e709ff0647543a0b122ae07197f0b324b4
fald <felipeaugustolira2006@gmail.com>
2026-05-23 17:49:38 -0300

    ainda trabalhando no mapa e iluminacao

---
+++ unstable_ground.gd
@@ -0,0 +1,17 @@
+extends RigidBody3D
+
+func _ready() -> void:
+	freeze = true
+	body_entered.connect(_on_body_entered)
+
+
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("Player") or body.name == "Player":
+		freeze = false
+		$Timer.start()
+
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
